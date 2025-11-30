57366e2dbc25c0cf29e270d3bdc3ee608ced97b1
anas.zemmouri@thomasmore.be <info@zemconsulting.be>
2025-11-30 20:46:01 +0100

    add code file

---
+++ code/main.py
@@ -0,0 +1,24 @@
+import emoji
+
+wordToEmoji = {
+    'super': ':thumbs_up:',
+    'coeur': ':red_heart:',
+    'sourire': ':smiley:',
+}
+
+
+def translate(word):
+    if word in wordToEmoji.keys():
+        return wordToEmoji[word]
+    else:
+        return word
+
+
+def convert(word):
+    # emoji.emojize convertit le code smiley en smileys
+    return emoji.emojize(word, use_aliases=True)
+
+
+if __name__ == "__main__":
+
+    print("super")
